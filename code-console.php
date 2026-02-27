d3d12302b29fed6a17a29561b9aefed86b8df563
AlAtaKe (Luis Enrique GA) <lenriquega@gmail.com>
2026-02-26 20:24:03 -0600

    Recuperando estilos perdidos

--- code-console.php
+++ code-console.php
@@ -4,7 +4,7 @@
  * Description:       Example block scaffolded with Create Block tool.
  * Version:           1.0.0
  * Requires at least: 6.8
- * Requires PHP:      8.3
+ * Requires PHP:      8.1
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
