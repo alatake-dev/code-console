6bb52a1a0343fcb23f66d544c0beaa934c7b5826
AlAtaKe (Luis Enrique GA) <lenriquega@gmail.com>
2026-04-03 12:25:24 -0600

    WIP: modificación de clases para estandarización

--- includes/class-block-registry.php
+++ includes/class-block-registry.php
@@ -4,13 +4,11 @@ namespace Triskelion\CodeConsole;
 class Block_Registry {
 
 	public static function init() {
-		self::block_init();
-		// Something....
+		add_action( 'init', [ __CLASS__, 'register_blocks' ] );
 	}
 
-	static function  block_init() {
-		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
-		add_action( 'init', 'triskelion_code_console_block_init' );
+	static function  register_blocks() {
+		register_block_type( TSK_CC_PATH . 'build/code-console' );
 	}
 
 }
